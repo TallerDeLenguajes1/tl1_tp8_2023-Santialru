59fc44b76b7e3b350a1f97ef4eae1f17df9a23ac
Santiago <alvarezrubiosanti@gmail.com>
2023-06-15 13:04:00 -0300

    primercommit

---
+++ practico8/Program.cs
@@ -0,0 +1,27 @@
+﻿using lasTareas;
+using System;
+
+List<tareas> TodasTareas = new List<tareas>();
+List<tareas> TareasPendientes = new List<tareas>();
+List<tareas> TareasRealizadas = new List<tareas>();
+
+Console.WriteLine("Cuantas tareas desea cargar?");
+int canti = int.Parse(Console.ReadLine());
+
+string[] tipo = {"limpiar", "reponer", "contar"};
+
+
+for (int i = 0; i < canti; i++)
+{
+    Random random = new Random();
+    tareas band = new tareas();
+    band.TareaID = i;
+    band.Duracion = random.Next(10,100);
+    band.Descripcion = tipo[1];
+    TodasTareas.Add(band);
+}
+
+foreach (tareas tarea in TodasTareas)
+{
+    Console.WriteLine(tarea);
+}
